0146879ee413453807d8b240cf775a6818bfb2a1
Pabl09 <munozjuanpablo424@gmail.com>
2024-09-09 13:53:45 -0600

    Se actualiza proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Home from './pages/Home'
+import CreateProduct from './pages/CreateProduct'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Item>
+                <Link to='/'>Home</Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link to='/create'>Crear</Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create' element={<CreateProduct />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/CreateProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateProduct = () => {
+  return (
+    <div>
+        <h1>Formulario</h1>
+    </div>
+  )
+}
+
+export default CreateProduct
